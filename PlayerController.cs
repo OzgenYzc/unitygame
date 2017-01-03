69a98c02eddd4fc393eee56aadebf65d0581978f
OzgenYzc <ozgen_yzc@hotmail.com>
2017-01-04 00:17:30 +0200

    Add files via upload
    Player Controller , move and clamp

---
+++ PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed=5f;
+	public float jumpSpeed=15f;
+	public float gravity=10f;
+	// Use this for initialization
+
+	private Vector2 moveDirection = Vector2.zero;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+
+
+	void FixedUpdate(){
+		
+		Rigidbody2D controller = GetComponent<Rigidbody2D> ();
+
+
+		moveDirection = new Vector2 (Input.GetAxis("Horizontal"),0);
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= speed;
+
+		transform.position= new Vector2(Mathf.Clamp (transform.position.x, -9.00f, 9.00f),transform.position.y);
+
+
+
+
+		if (Input.GetButton ("Jump")) {
+		
+			moveDirection.y = jumpSpeed;
+		
+		}
+
+		controller.velocity = moveDirection * speed;
+
+
+	}
+	void Update () {
+		
+		
+		}
+	}
+
